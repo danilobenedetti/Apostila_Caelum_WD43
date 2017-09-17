799b5b32eaa2832b1a2afcf85e3e0c730dfed776
Danilo Correa de Benedetti <danilo.bene@uol.com.br>
2017-09-16 23:55:49 -0300

    Apostila Caelum WD43
    Concluído até o capítulo 7

---
+++ capitulo_7/produto.php
@@ -0,0 +1,8 @@
+		<?php 
+			$cabecalho_title = "Produto Mirror Fashion";
+			$cabecalho_css = '<link rel="stylesheet" type="text/css" href="css/produto.css">';
+			include("cabecalho.php"); 
+		?>
+		<?php include("rodape.php"); ?>
+	</body>
+</html>
